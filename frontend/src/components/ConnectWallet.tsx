990ff5bf99bb21dd571d5a84048ec276d16c132a
ablin <space6fic@gmail.com>
2022-06-04 19:29:46 +0200

    Added autoconnect

--- frontend/src/components/ConnectWallet.tsx
+++ frontend/src/components/ConnectWallet.tsx
@@ -1,26 +1,67 @@
+import React, { useEffect } from "react";
 import { ethers } from "ethers";
+import detectEthereumProvider from "@metamask/detect-provider";
 import "../App.css";
 
 //TODO type
 const ConnectWallet = ({ userAddress, setUserAddress }: any) => {
-    console.log()
+  //   useEffect(() => {
+  //     const getProvider = async () => {
+  //       const provider = await detectEthereumProvider();
+
+  //       if (provider) {
+  //           console.log(window.ethereum.networkVersion)
+  //         if (window.ethereum.networkVersion !== "3") {
+  //           alert("Please switch to ropsten network"); // TODO use alert component
+  //           return;
+  //         }
+  //         const accounts = await window.ethereum.request({
+  //           method: "eth_requestAccounts",
+  //         });
+  //         if (accounts.length === 0) {
+  //           console.log("Please connect to MetaMask.");
+  //         } else if (accounts[0] !== userAddress) setUserAddress(accounts[0]);
+  //       } else alert("Please install MetaMask!");
+  //     };
+
+  //     getProvider();
+  //   }, []);
+
+  useEffect(() => {
+    const autoConnect = async () => {
+      if (window.ethereum && window.ethereum.networkVersion !== "3") {
+        const provider = new ethers.providers.Web3Provider(
+          window.ethereum,
+          "ropsten"
+        );
+
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setUserAddress(address);
+      }
+    };
+
+    autoConnect();
+  }, []);
+
   const connectWallet = async () => {
     if (window.ethereum) {
       if (window.ethereum.networkVersion !== "3") {
         alert("Please switch to ropsten network"); // TODO use alert component
         return;
       }
+
       const provider = new ethers.providers.Web3Provider(
         window.ethereum,
         "ropsten"
       );
-      console.log(provider);
+
       // Prompt user for account connections
       await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner();
       const address = await signer.getAddress();
       setUserAddress(address);
-      console.log("Account:", address);
     } else alert("Metamask Not Detected"); // TODO use alert component
   };
 
