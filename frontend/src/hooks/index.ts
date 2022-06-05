f3d800f87549a318524697148295e78e018b4f38
ablin <space6fic@gmail.com>
2022-06-05 03:56:42 +0200

    Added basic minting handling

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,11 +1,15 @@
-// hooks/index.ts
 import { ethers } from "ethers";
 import { useContractFunction } from "@usedapp/core";
 import { NFT_ABI } from "../ABI/NFT";
 import { NFT_CONTRACT_ADDRESS } from "../constant.js";
 
 const contractInterface = new ethers.utils.Interface(NFT_ABI);
-const contract = new ethers.Contract(NFT_CONTRACT_ADDRESS, contractInterface);
+
+// ? Forced to use any here due to a weird issue
+const contract: any = new ethers.Contract(
+  NFT_CONTRACT_ADDRESS,
+  contractInterface
+);
 
 export function useMint() {
   const { state, send } = useContractFunction(contract, "safeMint", {});
